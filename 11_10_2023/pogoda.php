d245ed7e0ed6380d7671fe727790a05c95bea492
appstore54 <jakub.mocko@onet.pl>
2023-10-11 14:30:57 +0200

    kw

---
+++ 11_10_2023/pogoda.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl2.css">
+    <title>Prognoza pogody Wrocław</title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="logo.png" alt="meteo">
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <div class="map">
+
+    </div>
+    <footer>
+
+    </footer>
+</body>
+</html>
