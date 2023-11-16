cbc9d7fe4d033885651e7199fb1a4846dca443a3
olganyga <19c.olga.nyga@ezse.pl>
2023-11-16 14:08:47 +0100

    egz_1

--- 15_11_2023/egzaminacyjne1.php
+++ 15_11_2023/egzaminacyjne1.php
@@ -9,16 +9,16 @@
     <h1>Formularz</h1>
     <form method="post" action="ankieta.php">
     <label>rok urodzenia</label>
-    <input type="number" name="rok" maxlength="4" require>
+    <input type="number" name="rok" maxlength="4" required>
 </br></br>
     <label>płeć</label>
-    <input type="text" name="plec" require>
+    <input type="text" name="plec" required>
 </br></br>
     <label>uwagi o stronie (max 100 znaków)</label>
-    <input type="text" name="uwagi" maxlength="100" require>
+    <input type="text" name="uwagi" maxlength="100" required>
 </br></br>
     <label>wyrażam zgodę na udział w badaniu</label>
-    <input type="checkbox" name="zgoda" require>
+    <input type="checkbox" name="zgoda" required>
 </br></br>
     <input type="submit">
 </form>
