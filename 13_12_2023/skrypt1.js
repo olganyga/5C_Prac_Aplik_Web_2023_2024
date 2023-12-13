2fc39e8f1f93a2d8c2963be55a1f5bd40de2d73d
olganyga <19c.olga.nyga@ezse.pl>
2023-12-13 15:14:26 +0100

    1-5

---
+++ 13_12_2023/skrypt1.js
@@ -0,0 +1,8 @@
+function zliczanie(){
+var ch = document.getElementById('txta').value.length
+console.log(ch)
+document.getElementById('izn').innerHTML = "Ilość wprowadzonych znaków : " + ch;
+
+
+
+}
